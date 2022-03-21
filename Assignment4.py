57bd81d69a2ccf06ff28bf5bcf9189cd16f0844b
ROCCYK <82534598+ROCCYK@users.noreply.github.com>
2022-03-21 18:22:59 -0400

    Update Assignment4.py

--- Assignment4.py
+++ Assignment4.py
@@ -34,9 +34,9 @@ def predict(data, X, X_label, Y, Y_label):
     dic[opposite] = 1 - bayest
     print(f'P({Y_label}|{X_label}):')
     if dic[Y_label] > dic[opposite]:
-        answer = Y_label
+        answer = Y_label + ' is a higher probability in terms of playing.'
     else:
-        answer = opposite
+        answer = opposite + ' is a higher probability in terms of playing.'
     return (dic, answer)
 
 def bayes4(data, X, X_label, Y, Y_label, X2, X_label2, X3, X_label3, X4, X_label4):
@@ -57,9 +57,9 @@ def predict4(data, X, X_label, Y, Y_label, X2, X_label2, X3, X_label3, X4, X_lab
     dic[opposite] = 1-bayest
     print(f'P({Y_label}|{X_label, X_label2, X_label3, X_label4 }):')
     if dic[Y_label] > dic[opposite]:
-        answer = Y_label
+        answer = Y_label + ' is a higher probability in terms of playing.'
     else:
-        answer = opposite
+        answer = opposite + ' is a higher probability in terms of playing.'
     return dic, answer
 
 #Tests
